53b466365861b73ea344a619e6b823dc49afb421
Daniel Aguirre <danter1@gmail.com>
2018-02-02 00:08:31 -0600

    Added db config and setup class

--- db/db.go
+++ db/db.go
@@ -8,3 +8,37 @@ import (
 var (
 	DBCon *sql.DB
 )
+
+type Person struct {
+	Id       string
+	Name     string
+	LastName string
+}
+
+func InitDb(filepath string) *sql.DB {
+	db, err := sql.Open("sqlite3", filepath)
+	if err != nil {
+		panic(err)
+	}
+
+	if db == nil {
+		panic("db nil")
+	}
+	return db
+}
+
+func CreateTable(db *sql.DB) {
+	// Create table if not exists
+	personTable := `
+	CREATE TABLE IF NOT EXISTS person(
+		Id TEXT NOT NULL PRIMARY KEY,
+		Name TEXT,
+		LastName Text,
+		created_at Date
+	)
+	`
+	_, err := db.Exec(personTable)
+	if err != nil {
+		panic(err)
+	}
+}
