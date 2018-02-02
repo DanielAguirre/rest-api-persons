bf760febc68743d31c1f2a410319046222306d43
Daniel Aguirre <danter1@gmail.com>
2018-02-01 22:45:27 -0600

    Created the set up databases  to connect and create a able for the Persons models

--- main.go
+++ main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"fmt"
+	"github.com/daniel/rest-api-persons/db"
+	"github.com/daniel/rest-api-persons/person"
+	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
 )
 
 func main() {
+	db.DBCon = db.InitDb("persosns.db")
+	defer db.DBCon.Close()
 
+	db.CreateTable(db.DBCon)
+	router := httprouter.New()
+	router.GET("/api/person/:id", person.FindPerson)
+	router.POST("/api/person", person.CreatePerson)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
