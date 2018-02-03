fd49a0e74f3788d7d93246a73c55788eefbe7f89
Daniel Aguirre <danter1@gmail.com>
2018-02-03 00:52:39 -0600

    :zap: Calling CreatePerson fucntion to record anew  resource in the DB

--- person/person.go
+++ person/person.go
@@ -28,6 +28,7 @@ func CreatePerson(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 	person.Id = uuid.NewV4()
 	fmt.Println(person.Name)
+	DBPerson.CreatePerson(person)
 	personJson, err := json.Marshal(person)
 	if err != nil {
 		panic(err.Error())
@@ -35,4 +36,41 @@ func CreatePerson(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	w.Write(personJson)
+}
+
+func FindPerson(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	id := params.ByName("id")
+	person = DBPerson.FindPerson(id)
+
+	personJson, err := json.Marshal(person)
+	if err != nil {
+		panic(err.Error())
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(personJson)
+}
+
+func UpdatePerson(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	err := json.NewDecoder(r.Body).Decode(&person)
+	DBPerson.UpdatePerson(person)
+	personJson, err := json.Marshal(person)
+	if err != nil {
+		panic(err.Error())
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(personJson)
+}
+
+func FindAllPeople(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var people []DBPerson.Person
+	people = DBPerson.FindAll()
+	peopleJson, err := json.Marshal(people)
+	if err != nil {
+		panic(err.Error())
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(peopleJson)
 }
